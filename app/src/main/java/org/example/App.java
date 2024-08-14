71af7ab4443c700cc0a1ba74ad6541f89b11ec90
Trevor Bernard <trevor.bernard@gmail.com>
2024-08-14 08:53:13 -0400

    Modify App.java to be a Router servre

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,42 @@
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import static org.zeromq.SocketType.ROUTER;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        var endpoint = "tcp://*:" + 60000;
+        try (var context = new ZContext();
+             var router = context.createSocket(ROUTER)) {
+            // Initialize router before bind
+            router.setTCPKeepAlive(1);
+            router.setTCPKeepAliveCount(1);
+            router.setTCPKeepAliveIdle(8);
+            router.setTCPKeepAliveInterval(5);
+            router.setLinger(0);
+            router.setHandshakeIvl(0);
+            router.setSendTimeOut(5000);
+            router.setReceiveTimeOut(5000);
+            router.bind(endpoint);
+
+            // Register socket to poller of size 1
+            try (var poller = context.createPoller(1)) {
+                poller.register(router, ZMQ.Poller.POLLIN);
+                // Find out if we officially support interrupting threads
+                while (!Thread.currentThread().isInterrupted()) {
+                    poller.poll(500);
+                    if (poller.pollin(0)) {
+                        // Receive message
+                    }
+                }
+                System.out.println("Main thread interrupted. Closing.");
+            }
+        } catch (Exception e) {
+            System.err.println("Context or Socket unexpectedly closed..");
+            e.printStackTrace();
+        }
+        System.out.println("All Jeromq resources have been successfully released");
     }
 }
